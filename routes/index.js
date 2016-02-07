9c1fa0f279d1e3e6ced06f4baefc81ea32886b75
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-02-07 17:31:08 -0500

    added contact and about me pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sukhmanbir | Home' });
 });
 
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Sukhmanbir | About Me' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Sukhmanbir | Contact Me' });
+});
+
 module.exports = router;
