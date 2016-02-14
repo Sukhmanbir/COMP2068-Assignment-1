2e593ffa5805ecba70e52ab48f097e2c44fa990a
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-02-14 00:31:17 -0500

    added wow.js

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sukhmanbir | Home' });
+  res.render('index', { title: 'Home' });
 });
 
 /* GET about me page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'Sukhmanbir | About Me' });
+router.get('/#about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/#projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+
+/* GET services page. */
+router.get('/#services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Sukhmanbir | Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
