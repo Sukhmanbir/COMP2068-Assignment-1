c1bac30cd53e0751770941d2f54af097b739158c
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-02-14 22:16:15 -0500

    added content and styles

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,10 @@
+/*
+    Filename : app.css
+    Author name: Sukhmanbir Kaur
+    Website name: Portfolio
+    Description: This is custom JavaScript file of the website
+*/
+
 (function(){
     console.log("App Started...");
     
